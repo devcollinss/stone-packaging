c42122be8db607400d810dec496d669e8ac46f8a
Collins Ikechukwu <collinschristroa@gmail.com>
2024-10-06 18:02:36 +0000

    test

--- package_alpine.sh
+++ package_alpine.sh
@@ -1,8 +1,37 @@
-#!/bin/sh
+#!/bin/bash
 
-TAG=$1
+set -e
 
-apk add --no-cache --virtual .build-deps alpine-sdk
-mv /app/prover /app/prover-${TAG}
-tar -czf prover-${TAG}.tar.gz prover-${TAG}
-mv prover-${TAG}.tar.gz /app/
+# Define package name and version from the tag
+PACKAGE_NAME="stone-prover"
+VERSION=$1  # Get the version from the argument
+
+# Create a directory for the Alpine package
+mkdir -p build/${PACKAGE_NAME}
+cd build/${PACKAGE_NAME}
+
+# Create the APKBUILD file
+cat <<EOF > APKBUILD
+# Maintainer: Your Name <your.email@example.com>
+pkgname=${PACKAGE_NAME}
+pkgver=${VERSION}
+pkgrel=0
+pkgdesc="Stone Prover for Tezos"
+url="https://github.com/baking-bad/stone-prover"
+arch="x86_64"
+license="MIT"
+depends=""
+source="https://github.com/baking-bad/stone-prover/archive/refs/tags/\$pkgver.tar.gz"
+
+build() {
+    cmake . || return 1
+    make || return 1
+}
+
+package() {
+    install -Dm755 your_binary -t "\$pkgdir/usr/bin"  # Adjust this line based on your output binary
+}
+EOF
+
+# Build the package
+abuild -r
