4e1037a0ec827108e3c676e0d8112f97ba3271be
Collins Ikechukwu <collinschristroa@gmail.com>
2024-10-08 08:35:25 +0000

    "Refactor release-alpine.yml workflow and package_alpine.sh script to use Docker and improve Alpine package creation"

--- package_alpine.sh
+++ package_alpine.sh
@@ -1,24 +1,20 @@
 #!/bin/sh
-
 set -e
 
 # Ensure the TAG argument is provided
 TAG=$1
 if [ -z "$TAG" ]; then
-  echo "Error: Tag argument is required"
-  exit 1
+    echo "Error: Tag argument is required"
+    exit 1
 fi
-
-# Install necessary Alpine packages for building
-apk add --no-cache alpine-sdk abuild
 
 # Create a temporary directory for the package
 BUILD_DIR="/tmp/stone-prover"
 mkdir -p $BUILD_DIR/usr/bin
 
 # Copy the binaries into the package directory
-cp /usr/local/bin/cpu_air_prover $BUILD_DIR/usr/bin/
-cp /usr/local/bin/cpu_air_verifier $BUILD_DIR/usr/bin/
+cp /work/cpu_air_prover $BUILD_DIR/usr/bin/
+cp /work/cpu_air_verifier $BUILD_DIR/usr/bin/
 
 # Create the APKBUILD file
 APKBUILD_FILE="$BUILD_DIR/APKBUILD"
@@ -27,24 +23,24 @@ cat <<EOF > $APKBUILD_FILE
 pkgname=stone-prover
 pkgver=$(echo $TAG | cut -c 2-)
 pkgrel=0
-arch="${TARGET_ARCH}"
-url="https://example.com" # Replace with the actual URL
-license="MIT" # Update with the correct license if different
+pkgdesc="Stone Prover binaries"
+url="https://github.com/yourusername/stone-prover"
+arch="x86_64"
+license="MIT"
 depends="libdw"
-
-build() {
-    # No build step needed since we're copying precompiled binaries
-    return 0
-}
+options="!check"
 
 package() {
-    install -Dm755 usr/bin/cpu_air_prover "${pkgdir}/usr/bin/cpu_air_prover"
-    install -Dm755 usr/bin/cpu_air_verifier "${pkgdir}/usr/bin/cpu_air_verifier"
+    install -Dm755 \$srcdir/usr/bin/cpu_air_prover "\$pkgdir/usr/bin/cpu_air_prover"
+    install -Dm755 \$srcdir/usr/bin/cpu_air_verifier "\$pkgdir/usr/bin/cpu_air_verifier"
 }
 EOF
 
 # Change to the build directory
 cd $BUILD_DIR
 
-# Create the APK package
+# Build the package
 abuild -r
+
+# Move the package to the mounted directory
+mv /home/builder/packages/work/*/*.apk /work/
